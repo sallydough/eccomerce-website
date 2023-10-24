2681493a2a76dab0bd1d359895c2977a036115c7
Sally Do <56125850+sallydough@users.noreply.github.com>
2023-10-24 10:46:23 -0600

    active burger menu

--- index.js
+++ index.js
@@ -2,3 +2,16 @@
 const bar = document.getElementById('bar');
 //Grab HTML element with id of 'navbar'
 const nav = document.getElementById('navbar');
+
+//1. Check if Navbar is already opened or not
+// if navbar not open - then if hamburger menu clicked, open nav bar
+//if navbar is opened - then if hamburger menu clicked, close/hide nav bar
+
+//add event listener to listen for if the hamburger menu is clicked
+//if anyone clicked on the bar/burger menu - it will become active
+
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add("active");
+  })
+}
